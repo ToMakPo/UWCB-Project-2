282458d8ac3f446bb56afa8032b627ff3caf6b0a
Elizabeth Brandt <ebrandt8@uw.edu>
2021-01-29 20:28:30 -0800

    fixed linting

--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -1,115 +1,105 @@
-
 const db = require("../models");
 const router = require("express").Router();
 const passport = require("../config/passport");
 
 router.post("/api/login", passport.authenticate("local"), (req, res) => {
-
   res.json({
     email: req.user.email,
     id: req.user.id
   });
 });
 
-
 router.post("/api/signup", (req, res) => {
-  db.userProfile.create({
-    email: req.body.email,
-    password: req.body.password,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name
-  })
+  db.userProfile
+    .create({
+      email: req.body.email,
+      password: req.body.password,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name
+    })
     .then(() => {
       res.redirect(307, "/api/login");
     })
     .catch(err => {
       res.status(401).json(err);
     });
+});
 
-  });
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-  router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
+router.get("/api/user_data", (req, res) => {
+  if (!req.user) {
+    res.json({});
+  } else {
+    res.json({
+      email: req.user.email,
+      id: req.user.id,
+      first_name: req.user.first_name,
+      last_name: req.user.last_name
+    });
+  }
+});
 
-
-
-  router.get("/api/user_data", (req, res) => {
-    if (!req.user) {
-      res.json({});
-    } else {
-
-      res.json({
-        email: req.user.email,
-        id: req.user.id,
-        first_name: req.user.first_name,
-        last_name: req.user.last_name
-      });
-    }
-  });
-
-  router.post("/api/bookList", (req, res) => {
-    db.bookList.create({
+router.post("/api/bookList", (req, res) => {
+  db.bookList
+    .create({
       google_book_id: req.body.google_book_id,
       completed: req.body.completed,
       ranking: req.body.ranking,
       userProfileId: req.body.userProfileId
-    
+  })
+    .then(() => {
+      res.send("ok");
     })
-      .then(() => {
-        res.send("ok")
-      })
-      .catch(err => {
-        res.status(401).json(err);
-      });
-  
+    .catch(err => {
+      res.status(401).json(err);
     });
+});
 
-    router.post("/api/comment", (req, res) => {
-      db.user_comment.create({
-        googleBookId: req.body.googleBookId,
-        text: req.body.text,
-        displayed: req.body.displayed,
-        liked: req.body.liked,
-        disliked: req.body.disliked,
-      
-      })
-        .then(() => {
-          res.send("ok")
-        })
-        .catch(err => {
-          res.status(401).json(err);
-        });
-    
-      });
-          
-      const { Op } = require("sequelize");
-      router.get("/api/check/:userId/:googleId", function(req, res) {
-       
-          db.bookList.findAll({
-            where: {
-              [Op.and]: [
-                { userprofileid: req.params.userId },
-                { google_book_id: req.params.googleId }
-              ]
-            } 
-         
-        }).then(function(isExist) {
+router.post("/api/comment", (req, res) => {
+  db.user_comment
+    .create({
+    googleBookId: req.body.googleBookId,
+    text: req.body.text,
+      displayed: req.body.displayed,
+      liked: req.body.liked,
+      disliked: req.body.disliked
+  })
+    .then(() => {
+      res.send("ok");
+    })
+    .catch(err => {
+      res.status(401).json(err);
+    });
+});
+
+const { Op } = require("sequelize");
+router.get("/api/check/:userId/:googleId", (req, res) => {
+  db.bookList
+    .findAll({
+      where: {
+        [Op.and]: [
+          { userprofileid: req.params.userId },
+          { google_book_id: req.params.googleId }
+        ]
+    } 
+    })
+  }).then((isExist) => {
        
           res.json(isExist);
         });
-      });
+});
 
-      
+router.get("/api/bookList/:userid", (req, res) => {
+  db.bookList.findAll({
+      where: {
+        userProfileId: req.params.userid
+    }
 
-      router.get("/api/bookList/:userid", function(req, res) {
-        db.bookList.findAll({
-          where: {
-            userProfileId: req.params.userid
-          }
-
-         }).then(function(Data) {
+  }).then((Data) => {
     
             var aa=JSON.stringify(Data)
             var list=JSON.parse(aa)
@@ -117,38 +107,34 @@ router.post("/api/signup", (req, res) => {
            res.json(list)
         
           });
-       
-      });
+});
 
-      router.put("/api/bookList/:id/:isCompleted", function(req, res) {
-        db.bookList.update({completed:req.params.isCompleted},
-          
-          {
-            where: {
-              google_book_id: req.params.id
-            }
-          })
-          .then(function(data) {
+router.put("/api/bookList/:id/:isCompleted", (req, res) => {
+  db.bookList.update({completed:req.params.isCompleted},
+
+    {
+        where: {
+          google_book_id: req.params.id
+      }
+    })
+    .then((data) => {
             res.json(data);
           
             
           });
-      });
+});
 
-      router.delete("/api/bookList/:id/:userProfileId", function(req, res) {
-   
-        db.bookList.destroy({
-          where: {
-            google_book_id: req.params.id,
-            userProfileId:req.params.userProfileId
+router.delete("/api/bookList/:id/:userProfileId", (req, res) => {
+  db.bookList
+    .destroy({
+      where: {
+        google_book_id: req.params.id,
+      userProfileId:req.params.userProfileId
 
-          }
-        }).then(function(book) {
+    }
+  }).then((book) => {
           res.json(book);
         });
-    
-      });
-     
-
+});
 
 module.exports = router;
--- sockets.js
+++ sockets.js
@@ -1,16 +1,15 @@
-const socket = require('socket.io')
+const socket = require("socket.io");
 
 module.exports = function(server) {
-    const io = socket(server)
-    io.sockets.on('connection', newConnection)
+  const io = socket(server);
+  io.sockets.on("connection", newConnection);
 
-    
-    function newConnection(socket) {
-        console.log('new connection: ', socket.id)
-        
-        socket.on('send-msg', data => {
-            console.log(`USER ${data.userID} >>> sent-msg`);
-            socket.broadcast.emit(`${data.bookID}-sent-msg`, data)
-        })
-    }
-}
+  function newConnection(socket) {
+    console.log("new connection: ", socket.id);
+
+    socket.on("send-msg", data => {
+      console.log(`USER ${data.userID} >>> sent-msg`);
+      socket.broadcast.emit(`${data.bookID}-sent-msg`, data);
+    });
+  }
+};
